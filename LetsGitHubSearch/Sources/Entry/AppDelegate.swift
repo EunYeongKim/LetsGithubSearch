6b59424a1089752fc14737895d02749f7d70eb3b
김은영 <povoq1024@mail.shinhan.dev>
2021-04-27 17:23:05 +0900

    첫 커밋 * devxoul/LetsGitHubSearch 참고

---
+++ LetsGitHubSearch/Sources/Entry/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  LetsGitHubSearch
+//
+//  Created by Suyeol Jeon on 03/11/2018.
+//  Copyright © 2018 Suyeol Jeon. All rights reserved.
+//
+
+import UIKit
+
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  var window: UIWindow?
+
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    
+    return true
+  }
+}
+
